e35235a310e8283e788455a2ed2d4282da04e7b6
ishitapareek <ishitapareek15@gmail.com>
2023-08-31 21:01:46 +0530

    added reservation insert and clear button

--- UI/Reservation.py
+++ UI/Reservation.py
@@ -7,43 +7,61 @@ root = Tk ()
 
 root.title ('Reservation')
 
-def button_clear():
-    a = 10
-    return a
+def Clear():
+    txtName.delete(0, 'end')
+    txtDOB.delete(0, 'end')
+    txtMobile.delete(0, 'end')
+    txtEmail.delete(0, 'end')
+    txtAddress.delete(0, 'end')
+    
+    txtDates.delete(0, 'end')
+    txtDays.delete(0, 'end')
+    clickedBoarding.set('')
+    clickedLeaving.set('')
+    txtMembers.delete(0, 'end')
 
 
-def Save(Name, DOB, PhoneNo, Email, Address):
+def Save(Name, DOB, PhoneNo, Email, Address, Dates, Days, Boarding, Leaving, Members):
     dbconnection = mysql.connector.connect (host = 'localhost', username = 'root', password = 'admin', database = 'class_xii_project')
     dbcursor = dbconnection.cursor()
+    
+    InsertQueryPersonal = "insert into personal (Name, DOB, Mobile, Email, Address) values ('" + Name + "', '" + DOB + "', '" + PhoneNo + "', '" + Email + "', '" + Address + "')"
+    dbcursor.execute (InsertQueryPersonal)
+    RegNo = str(dbcursor.lastrowid)
 
+    InsertQueryReservation = "insert into Reservation values ('" + str(dbcursor.lastrowid) + "', '" + Dates + "', '" + Days + "', '" + Boarding + "', '" + Leaving + "', '" + Members + "')"
+    dbcursor.execute (InsertQueryReservation)
     
-    InsertQuery = "insert into personal (Name, DOB, Mobile, Email, Address) values ('" + Name + "', '" + DOB + "', '" + PhoneNo + "', '" + Email + "', '" + Address + "')"
-    
-    dbcursor.execute (InsertQuery)
     dbconnection.commit()
 
 
-    Msg = Name + ', your reservation has been confirmed.' + '\n \n' + 'Your registration number is: ' + str(dbcursor.lastrowid)
+    messagebox.askquestion("Are You Sure?", "Do you want to confirm your reservation?")
+
+    Msg = Name + ', your reservation has been confirmed.' + '\n \n ' + 'Your registration number is: ' + RegNo
     messagebox.showinfo('Confirmation', Msg)
+    
+
     
 def GetValues():
     Name = txtName.get()
     DOB = txtDOB.get()
-    PhoneNo = txtMoblie.get()
+    PhoneNo = txtMobile.get()
     Email = txtEmail.get()
     Address = txtAddress.get()
 
     Dates = txtDates.get()
     Days = txtDays.get()
-    #Boarding = dropBoarding.get()
-    #Leaving = dropLeaving.get()
+    Boarding = clickedBoarding.get()
+    Leaving = clickedLeaving.get()
     Members = txtMembers.get()
-    
-    Save(Name, DOB, PhoneNo, Email, Address)
+    Save(Name, DOB, PhoneNo, Email, Address, Dates, Days, Boarding, Leaving, Members)
+
     
 def CreateLabel(container, caption, r, c):
     label = Label (container, text = caption)
     label.grid (row = r, column = c)
+
+
 
 framePersonalInfo = LabelFrame(root, text = 'Personal Info', padx = 7, pady = 10)
 framePersonalInfo.grid(row = 0, column = 0, padx = 20, pady = 20)
@@ -59,8 +77,8 @@ txtDOB.grid (row = 0, column = 3)
 
 
 CreateLabel (framePersonalInfo, 'Phone Number: ', 1, 0)
-txtMoblie = Entry (framePersonalInfo)
-txtMoblie.grid (row = 1, column = 1)
+txtMobile = Entry (framePersonalInfo)
+txtMobile.grid (row = 1, column = 1)
 
 
 CreateLabel (framePersonalInfo, 'Email: ', 1, 2)
@@ -85,6 +103,7 @@ CreateLabel (frameReservationInfo, 'Days: ', 0, 2)
 txtDays = Entry (frameReservationInfo)
 txtDays.grid (row = 0, column = 3)
 
+
 clickedBoarding = StringVar()
 clickedLeaving = StringVar()
 
@@ -108,7 +127,7 @@ txtMembers.grid (row = 2, column = 1)
 frameAction = LabelFrame (root, padx = 7, pady = 10)
 frameAction.grid(row = 2, column = 0, padx = 20, pady = 20)
    
-btnClear = Button (frameAction, text = 'Clear', command = button_clear)
+btnClear = Button (frameAction, text = 'Clear', command = Clear)
 btnClear.grid(row = 0, column = 0)
 
     
